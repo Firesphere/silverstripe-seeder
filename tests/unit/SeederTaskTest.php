4acb83d48e434d78defaf9c5253f6a82d31dcb13
Simon Erkelens <simonerkelens@silverstripe.com>
2018-02-22 20:05:46 +1300

    Use statements. Again

--- tests/unit/SeederTaskTest.php
+++ tests/unit/SeederTaskTest.php
@@ -9,6 +9,7 @@ use SilverStripe\Control\Director;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Core\Kernel;
 use SilverStripe\Dev\SapphireTest;
 
 /**
